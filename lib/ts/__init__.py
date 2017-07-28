a24f64669b7295033169fe53902018d95250ee3f
eshikafe <eshikafe@gmail.com>
2017-07-28 21:59:56 +0100

    Modularie the ts folder

--- lib/ts/__init__.py
+++ lib/ts/__init__.py
@@ -1,3 +1,15 @@
-#__all__ = ["ts36211", "ts36212", "ts36213", "ts36214", "ts36216"]
+# 3GPP TS
+
+ts = {"ts36211": "LTE Physical Channel Modulation",\
+      "ts36212": "LTE Multiplexing Channel Coding", \
+      "ts36213": "LTE Physical Layer Procedures",   \
+      "ts36214": "LTE Phyiscal Layer Measurements", \
+      "ts36216": "LTE Physical Layer Relaying Operation",\
+      "ts36331": "LTE RRC Protocol", \
+      "ts36321": "LTE MAC Protocol",\
+      "ts36413": "LTE S1 Application Protocol",\
+      "ts29281": "LTE GTPv1 User Plane Protocol", \
+      "ts36323": "LTE PDCP Protocol"
+           }
 
 
