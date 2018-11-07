86e01514f570b86b5689ffbfa1129c1cdedf8aff
Austin Aigbe <eshikafe@gmail.com>
2018-11-07 19:22:36 +0100

    Relocate TS 36.101 file

---
+++ src/pdcp.erl
@@ -0,0 +1,10 @@
+% Copyright (c) 2018 Aigbe Research
+% pdcp.erl
+% 
+% 3GPP LTE Packet Data Convergence Protocol
+% 3GPP TS 36.323
+
+-module(pdcp).
+
+-include("ts/lte_security_ts33401.hrl").
+-include("ts/lte_pdcp_ts36323.hrl").
