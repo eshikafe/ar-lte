c2fce0c5e547f211c4b48a44ae9600520fd611df
Austin Aigbe <eshikafe@gmail.com>
2018-11-07 19:20:21 +0100

    Update src/ts/lte_security_ts33401.erl

---
+++ src/ts/lte_security_ts33401.erl
@@ -0,0 +1,47 @@
+% Copyright (c) 2018 Aigbe Research
+% lte_security_ts33401.erl
+%
+% Compliance: 
+%    3GPP TS 33.401 version 12.13.0 Release 12
+%    3GPP TS 33.220 V12.3.0
+%    3GPP TS 35.215 V15.0.0 (2018-06)
+
+-module(lte_security_ts33401).
+
+-export([kdf/3]).
+
+-export([eea2_128_encryption/6, eia2_128_integrity/6]).
+
+% Use the sha256() hash function to compute a hmac code with the hmac() function
+hmac_sha256(Key, Data) ->
+	crypto:hmac(sha256, Key, Data, 16).
+
+% key derivation function
+%   output keys: KRRCint, KRRCenc, KUPint, KUPenc
+kdf(KeNB, AlgoType, AlgoId) when size(KeNB) == 32 ->
+	FC = <<15:8>>, 
+	P0 = AlgoType,         % algorithm type distinguisher
+	L0 = <<1:16>>,         % length of algorithm type distinguisher (i.e 0x00 0x01)
+	P1= AlgoId,            % algorithm identity
+	L1 = <<1:16>>,         % length of algorithm identity (i.e. 0x00 0x01)
+
+    % S = FC || P0 || L0 || P1 || L1 || P2 || L2 || P3 || L3 ||... || Pn || Ln */
+    S = <<FC/binary, P0/binary, L0/binary, P1/binary, L1/binary>>, 
+    hmac_sha256(K_eNB, S).
+
+% ciphering algorithm
+%  used by the PDCP layer to encrypt the data part of the PCDCP PDU
+%  Key: 128 bits
+%  Count: 32 bits
+%  Bearer: 5 bits
+%  Direction: 1 bit (0 - uplink, 1 - downlink)
+%  Length: length(Msg)
+eea2_128_enc(Key, Count, Bearer, Direction, Length, PlainText) ->
+	crypto:block_encrypt(ecb_aes, KeyStream, PlainText).
+
+eia2_128_int(Key, Count, Bearer, Direction, Length, PlainText) ->
+	todo.
+
+
+
+
