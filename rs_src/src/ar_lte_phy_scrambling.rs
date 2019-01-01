7dc1e046432ecc1a2bfb082038e544234a430b58
Austin Aigbe <eshikafe@gmail.com>
2019-01-01 19:58:48 +0100

    Applied cargo check fixes

--- rs_src/src/ar_lte_phy_scrambling.rs
+++ rs_src/src/ar_lte_phy_scrambling.rs
@@ -6,6 +6,6 @@ fn pseudo_rand_seq() {
     // TS 36.211 V12.2.0, section 7.2
    // Pseudo-random sequences are defined by _a length-31 Gold sequence
    i:u32;
-   x1:u32, n1, x2, n2:u32;
+   x1:u32, n1:u32, x2:u32, n2:u32;
 
 }
---
+++ rs_src/src/main.rs
@@ -0,0 +1,4 @@
+fn main() {
+    //todo
+    // target: src/lib.rs
+}
