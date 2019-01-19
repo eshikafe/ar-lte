1b61d0b24e57d45f900e06c1639546979728aab5
Austin Aigbe <eshikafe@gmail.com>
2019-01-19 19:09:55 +0100

    Update ar_lte_phy.rs

--- rs_src/src/ar_lte_phy.rs
+++ rs_src/src/ar_lte_phy.rs
@@ -1,5 +1,5 @@
-// Copyright (c) 2018 Aigbe Research
 // ar_lte_phy.rs - LTE Physical Layer
+// Copyright (c) 2018 Aigbe Research
 // Compliance: 
 //      3GPP TS 36.211  
 //      3GPP TS 36.212 
@@ -8,10 +8,10 @@
 //      3GPP TS 36.214 
 
 
-mod phy_layer_ts36211; // Physical channels and modulation
-mod phy_layer_ts36212; // Multiplexing and channel coding
-mod phy_layer_ts36213; // Physical layer for relaying operation
-mod phy_layer_ts36214; // Physical layer procedures
-mod phy_layer_ts36216; // Physical layer - Measurements
+mod lte_phy_layer_ts36211; // Physical channels and modulation
+mod lte_phy_layer_ts36212; // Multiplexing and channel coding
+mod lte_phy_layer_ts36213; // Physical layer for relaying operation
+mod lte_phy_layer_ts36214; // Physical layer procedures
+mod lte_phy_layer_ts36216; // Physical layer - Measurements
 
 
