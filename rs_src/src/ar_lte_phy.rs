6fcaa3f9db2c468def3685009c815d7696f64172
Austin Aigbe <eshikafe@gmail.com>
2019-01-19 13:21:33 +0100

    Rename file

--- rs_src/src/ar_lte_phy.rs
+++ rs_src/src/ar_lte_phy.rs
@@ -1,6 +1,10 @@
 // Copyright (c) 2018 Aigbe Research
-// ar_lte_phy_scrambling.rs
-// TS 36.211
+// ar_lte_phy.rs
+// LTE Physical Layer
+// Compliance: 3GPP TS 36.211
+
+
+const N_SYMB_DL_NORMAL_CP: u8 = 7;
 
 fn pseudo_rand_seq() {
     // TS 36.211 V12.2.0, section 7.2
