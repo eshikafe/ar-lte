b49556e17518e9e1235ebc552c2b9586dd8986cd
Austin Aigbe <eshikafe@gmail.com>
2019-02-05 11:00:30 +0100

    Create mod.rs

---
+++ rs_src/src/phy_layer/mod.rs
@@ -0,0 +1,17 @@
+// ar_lte_phy.rs - LTE Physical Layer
+// Copyright (c) 2018 Aigbe Research
+// Compliance: 
+//      3GPP TS 36.211  
+//      3GPP TS 36.212 
+//      3GPP TS 36.216 
+//      3GPP TS 36.213 
+//      3GPP TS 36.214 
+
+
+pub mod ts_36211; // Physical channels and modulation
+pub mod ts_36212; // Multiplexing and channel coding
+pub mod ts_36213; // Physical layer for relaying operation
+pub mod ts_36214; // Physical layer procedures
+pub mod ts_36216; // Physical layer - Measurements
+
+
