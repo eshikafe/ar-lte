8d9c883f146380dd6780ac06ed25f0e950d643de
Austin Aigbe <eshikafe@gmail.com>
2019-02-25 07:09:02 +0100

    Rename folder/file

--- rs_src/src/main.rs
+++ rs_src/src/main.rs
@@ -1,14 +1,14 @@
 #[macro_use]
 extern crate lazy_static;
 
-mod phy_layer;
+mod ngr_phy;
 
 fn main() {
 
     for _i in 0..64 {
         println!("64QAM Symbol {}: (I + jQ) = {} + {}j",_i, 
-        phy_layer::ts_36211::ModSym64Qam[_i].i,
-        phy_layer::ts_36211::ModSym64Qam[_i].q);
+        ngr_phy::ts_36211::ModSym64Qam[_i].i,
+        ngr_phy::ts_36211::ModSym64Qam[_i].q);
     }
    
 }
@@ -17,13 +17,13 @@ fn main() {
 // LTE Physical Layer procedure tests
 #[test]
 fn test_x1() {
-    let x1 = phy_layer::ts_36211::x_1();
+    let x1 = ngr_phy::ts_36211::x_1();
     //assert_eq!(x1,0x54d21b24);
     println!("x1 = 0x{:x}", x1);
 }
 
 #[test]
 fn test_x2() {
-    let x2 = phy_layer::ts_36211::x_2(100);
+    let x2 = ngr_phy::ts_36211::x_2(100);
     println!("x2 = 0x{:x}", x2);
 }
