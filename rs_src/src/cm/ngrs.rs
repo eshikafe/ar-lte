05928ca602799cd99daa354aa7a1f24ee898b898
Austin Aigbe <eshikafe@gmail.com>
2019-03-06 10:56:14 +0100

    Create ngr cli shell source file

--- rs_src/src/cm/ngrs.rs
+++ rs_src/src/cm/ngrs.rs
@@ -1,2 +1,13 @@
 // ngrs - ngr shell
 // Copyright (c) 2018 Aigbe Research
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+struct Cli {
+    /// prompt id
+    hostname: String,
+    /// The path to the file to read
+    #[structopt(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
