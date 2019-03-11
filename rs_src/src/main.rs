af21eb9f5221d8aa32ae1c1800345372ef7a4dd0
Austin Aigbe <eshikafe@gmail.com>
2019-03-11 20:21:43 +0100

    Update main.rs

--- rs_src/src/main.rs
+++ rs_src/src/main.rs
@@ -1,22 +1,8 @@
 //#[macro_use]
 //extern crate lazy_static;
 
-mod ngr_phy;
+// mod ngr_phy;
 
 fn main() {
-    test_x1();
-    test_x2();  
-}
 
-
-// LTE Physical Layer procedure tests
-fn test_x1() {
-    let x1 = ngr_phy::ts36211::x1();
-    //assert_eq!(x1,0x54d21b24);
-    println!("x1 = 0x{:x}", x1);
-}
-
-fn test_x2() {
-    let x2 = ngr_phy::ts36211::x2(100);
-    println!("x2 = 0x{:x}", x2);
 }
