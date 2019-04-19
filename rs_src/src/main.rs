d1497ea9ef9b07be32c61f9b5a8f93df9e2265c2
Austin Aigbe <eshikafe@gmail.com>
2019-04-19 14:22:26 +0100

    Update main.rs

--- rs_src/src/main.rs
+++ rs_src/src/main.rs
@@ -1,8 +1,19 @@
 //#[macro_use]
 //extern crate lazy_static;
+#[allow(dead_code)]
+#[allow(non_snake_case)]
+#[allow(unused_variables)]
+#[allow(non_upper_case_globals)]
 
-// mod ngr_phy;
+mod phy;
+
 
 fn main() {
+    let mut l1 = phy::PhysicalLayer::new();
+    
+    println!("\nBefore\ncodeword: {:?}\nscrambled bits {:?}", l1.codeword, l1.scrambled_bits);
+    phy::scrambling::run(&mut l1);
+    println!("\nAfter\ncodeword: {:?}\nscrambled bits {:?}", l1.codeword, l1.scrambled_bits);
 
 }
+
