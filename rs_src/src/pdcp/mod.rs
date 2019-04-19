a26f1d777f160c25252de75f497492da02917322
Austin Aigbe <eshikafe@gmail.com>
2019-04-19 18:02:29 +0100

    Rename pdcp module

---
+++ rs_src/src/pdcp/mod.rs
@@ -0,0 +1,9 @@
+// Copyright (c) 2016 Aigbe Research
+// Packet Data Convergence Protocol (PDCP) Implementation in Rust
+// LTE/5G
+//
+// Reference Document:
+//   3GPP TS 36.323, 3GPP Release 12.2.0
+ 
+
+pub mod ts_36_323;
