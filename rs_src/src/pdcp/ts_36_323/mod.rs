d8a70c7243412fe0367cd25b5a42ecfa951ef442
Austin Aigbe <eshikafe@gmail.com>
2019-04-19 18:11:47 +0100

    Update mod.rs

--- rs_src/src/pdcp/ts_36_323/mod.rs
+++ rs_src/src/pdcp/ts_36_323/mod.rs
@@ -1,3 +1,8 @@
 // Copyright (c) 2016 Aigbe Research
+// Packet Data Convergence Protocol (PDCP) Implementation in Rust
+// LTE/5G
+//
+// Reference Document:
+//   3GPP TS 36.323, 3GPP Release 12.2.0
 
 pub mod common;
