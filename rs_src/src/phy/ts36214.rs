09d5db7805c48456c56194216d63cde31df40587
Austin Aigbe <eshikafe@gmail.com>
2019-04-19 14:21:25 +0100

    Create ts36214.rs

---
+++ rs_src/src/phy/ts36214.rs
@@ -0,0 +1,11 @@
+// (c) 2019 Aigbe Research
+//  ts_36214.rs
+//  3GPP TS 36.214: Physical layer - Measurements
+//  The scope of this specification is to establish 
+//  the characteristics of the physical layer measurements, and to specify:
+//  - Measurements to be performed by Layer 1 in UE and E-UTRAN;
+//  - Reporting of measurement results to higher layers and the network;
+//  - Handover measurements, idle-mode measurements, etc.
+// 
+//  Reference: 3GPP TS 36.201 version 12.2.0 Release 12, Section 5.6
+
