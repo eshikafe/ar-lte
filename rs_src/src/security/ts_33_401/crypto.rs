5e03874f784c3273fbe2db313f73a72a8ea17ab7
Austin Aigbe <eshikafe@gmail.com>
2019-06-05 18:28:43 +0100

    Update crypto.rs

--- rs_src/src/security/ts_33_401/crypto.rs
+++ rs_src/src/security/ts_33_401/crypto.rs
@@ -5,65 +5,87 @@
 //    3GPP TS 33.220 V12.3.0
 //    3GPP TS 35.215 V15.0.0 (2018-06)
 
+use hmac_sha256::*;
 
 // Table A.7-1: Algorithm type distinguishers
-pub const ALGO_TYPE_NAS_ENC: u8 = 1;
-pub const ALGO_TYPE_NAS_INT: u8 = 2;
-pub const ALGO_TYPE_RRC_ENC: u8 = 3;
-pub const ALGO_TYPE_RRC_INT: u8 = 4;
-pub const ALGO_TYPE_UP_ENC: u8 = 5;
-pub const ALGO_TYPE_UP_INT: u8 = 6;
+// const ALG_TYPE_NAS_ENC: u8 = 0x01;
+// const ALG_TYPE_NAS_INT: u8 = 0x02;
+const ALG_TYPE_RRC_ENC: u8 = 0x03;
+const ALG_TYPE_RRC_INT: u8 = 0x04;
+const ALG_TYPE_UP_ENC: u8 = 0x05;
+const ALG_TYPE_UP_INT: u8 = 0x06;
 
 // 5.1.4.1 Integrity requirements
 // EPS Integrity Algorithm Identity
-pub const ALGO_ID_EIA0: u8 = 0;     // Null Integrity Protection algorithm
-pub const ALGO_ID_128_EIA1: u8 = 1; // SNOW 3G based algorithm
-pub const ALGO_ID_128_EIA2: u8 = 2; // AES based algorithm
-pub const ALGO_ID_128_EIA3: u8 = 3; // ZUC based algorithm
+const ALGO_ID_EIA0: u8 = 0;     // Null Integrity Protection algorithm
+const ALGO_ID_128_EIA1: u8 = 1; // SNOW 3G based algorithm
+const ALGO_ID_128_EIA2: u8 = 2; // AES based algorithm
+const ALGO_ID_128_EIA3: u8 = 3; // ZUC based algorithm
 
 // 5.1.3.2 Algorithm Identifier Values 
 // EPS Encryption Algorithm Identity
-pub const ALGO_ID_EEA0: u8 = 0; 
-pub const ALGO_ID_128_EEA1: u8 = 1;
-pub const ALGO_ID_128_EEA2: u8 = 2;
-pub const ALGO_ID_128_EEA3: u8 = 3;
+const ALGO_ID_EEA0: u8 = 0; 
+const ALGO_ID_128_EEA1: u8 = 1;
+const ALGO_ID_128_EEA2: u8 = 2;
+const ALGO_ID_128_EEA3: u8 = 3;
 
 
-// Use the sha256() hash function to compute a hmac code with the hmac() function
-// fn hmac_sha256(Key: Vec<128>, Data) -> Vec<u128> {
-// 	crypto:hmac(sha256, Key, Data, 16)
-// }
+fn kdf(key: &[u8], s: &[u8]) -> [u8; 32] {
+ 	let h = HMAC::mac(s, key);
+    return h; // 256 bits
+ }
 
 
-// Key Derivation Function (KDF)
-// Inputs: k_enb (256 bits), algo_type (8 bits), algo_id (8 bits)
-// Output keys (128 bits): KRRCint, KRRCenc, KUPint or KUPenc
-pub fn kdf(k_enb: Vec<u128>, algo_type: u8, algo_id: u8) -> u64 {
-	let fc: u64 = 15; 
-	let p0: u64 = algo_type as u64;  // algorithm type distinguisher
-	let l0: u64 = 1;                 // length of algorithm type distinguisher (i.e 0x00 0x01)
-	let p1: u64 = algo_id as u64;    // algorithm identity
-	let l1: u64 = 1;                 // length of algorithm identity (i.e. 0x00 0x01)
+// A.7 Algorithm Key Derivation Functions
+fn algo_kdf(k_enb: &[u8], algo_type: u8, algo_id: u8) -> [u8; 32] {
+	let fc: u8 = 0x15; 
+	let p0: u8 = algo_type;            // algorithm type distinguisher
+	let l0: [u8;2] = [0x00, 0x01];     // length of algorithm type distinguisher (i.e 0x00 0x01)
+	let p1: u8 = algo_id;              // algorithm identity
+	let l1: [u8;2] = [0x00, 0x01];     // length of algorithm identity (i.e. 0x00 0x01)
 
-    // S = FC || P0 || L0 || P1 || L1 || P2 || L2 || P3 || L3 ||... || Pn || Ln */
-    let s: u64 = (fc << 56 | p0 << 48 | l0 << 32 | p1 << 24 | l1 << 8) & 0xffffffffffffff00; 
-    //hmac_sha256(k_enb, s)
-    return s;
+    // S = FC || P0 || L0 || P1 || L1 || P2 || L2 || P3 || L3 ||... || Pn || Ln
+    let s = &[fc, p0, l0[0], l0[1], p1, l1[0], l1[1]];  
+    return kdf(k_enb, s);
 }
 
-// % ciphering algorithm
-// %  used by the PDCP layer to encrypt the data part of the PDCP PDU
-// %  Key: 128 bits
-// %  Count: 32 bits
-// %  Bearer: 5 bits
-// %  Direction: 1 bit (0 - uplink, 1 - downlink)
-// %  Length: length(Msg)
-// fn eea2_128_encrypt(Key: u128, Count: u32, Bearer: u8, Direction: u8, Length: u32, Data: Vec<u8>) -> u128 {
-// 	crypto:block_encrypt(ecb_aes, KeyStream, Data)
-// }
-// fn eia2_128_int(Key, Count, Bearer, Direction, Length, Data) -> u128 {
+
+pub fn k_rrc_int_key(k_enb: &[u8]) -> [u8; 16] {
+	let k = algo_kdf(k_enb, ALG_TYPE_RRC_INT, ALGO_ID_128_EIA2);
+	return k[16..32]; // 128 bits
+	
+}
+
+pub fn k_rrc_enc_key(k_enb: &[u8]) -> [u8; 16] {
+	let k = algo_kdf(k_enb, ALG_TYPE_RRC_ENC, ALGO_ID_128_EEA2);
+	return k[16..32];
+} 
+
+pub fn k_up_int_key(k_enb: &[u8]) -> [u8; 16] {
+	let k = algo_kdf(k_enb, ALG_TYPE_UP_INT, ALGO_ID_128_EIA2);
+	return k[16..32];
+}
+
+pub fn k_up_enc_key(k_enb: &[u8]) -> [u8; 16] {
+	let k = algo_kdf(k_enb, ALG_TYPE_UP_ENC, ALGO_ID_128_EEA2);
+	return k[16..32];
+}
+
+// Ciphering algorithm
+// used by the PDCP layer to encrypt the data part of the PDCP PDU
+// Key: 128 bits
+// Count: 32 bits
+// Bearer: 5 bits
+// Direction: 1 bit (0 - uplink, 1 - downlink)
+// Length: length(Msg)
+ //fn eea2_128_enc(key: &[u8], count: u32, bearer: u8, direction: bool, length: u32, data: &[u8]) {
+ //	
+ //}
+
+ // Integrity
+ //fn eia2_128_int(Key, Count, Bearer, Direction, Length, Data) -> u128 {
 // 	//todo.
-// }
+ //}//
 
 
 
